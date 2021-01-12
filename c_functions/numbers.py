8cc1d17e8138e2c6aaa43050fa638ac99be7f285
dmlee89 <ldmldm1119@gmail.com>
2021-01-12 18:18:41 +0900

    numbers_Math functions(Math library)

---
+++ c_functions/numbers.py
@@ -0,0 +1,12 @@
+from math import *  # math library
+# Number Functions
+print(abs(-5))  # 절대값 (=5)
+print(pow(4, 2))  # 4^2 (=16)
+print(max(2, 4, 15))  # 최대값 (=15)
+print(min(2, 4, 15))  # 최소값 (=2)
+print(round(3.14))  # 반올림(=3)
+print(round(4.68))  # 반올림(=5)
+
+print(floor(4.99))  # 버림(=4)
+print(ceil(3.14))  # 올림(=4)
+print(sqrt(16))  # 제곱근 (=4)
