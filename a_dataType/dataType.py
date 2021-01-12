b3c3a7b8c8d318e8670410e9b05c573a3c144cff
dmlee89 <ldmldm1119@gmail.com>
2021-01-12 18:04:59 +0900

    dataType_Number(int), String, Boolean

---
+++ a_dataType/dataType.py
@@ -0,0 +1,21 @@
+# Number
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+print(3*(3+1))
+# String
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+print("ㅋ"*9)
+# Boolean
+print(True)
+print(False)
+print(5 > 10)
+print(5 < 10)
+print(not False)
+print(not (5 < 10))
+
